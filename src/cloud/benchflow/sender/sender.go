232b688670f56df190b53e28d61e88db73eccf98
Cerfoglg <gabrielecerfoglio225@gmail.com>
2016-05-16 14:54:21 +0200

    using viper for configuration with override using ENV variables, commented code

--- src/cloud/benchflow/sender/sender.go
+++ src/cloud/benchflow/sender/sender.go
@@ -15,30 +15,52 @@ import (
 	"strings"
 	"bytes"
 	"time"
+	"github.com/spf13/viper"
 )
 
+// Concurrent map for counting how many trials have been performed for a certain experiment
 var trialCount = cmap.New()
-var mutex sync.Mutex
 
+// Configuration of the scripts and dependencies
 var c Configuration
+
+// Map that connects the string representing script requirements to the list of scripts that have those requirement as per configuration
 var reqScripts = make(map[string] []AnalyserScript)
+
+// Nested map that tracks for a given trialID and requirement if said requirement was met for that trial (if contains true, requirement is met)
+// Example: reqGroupDone[trial_1][cpu] = true
 var reqTracker = make(map[string] map[string] bool)
+
+// Nested map that tracks which group of scripts was launched for a given trial, indexed by trial and requirements string. 
+// Example: reqGroupDone[trial_1][cpu,ram] = true
 var reqGroupDone = make(map[string] map[string] bool)
+
+// Lists of all requirements
 var allRequirements []string
+
+//List of all scripts
 var allScripts []string
 
+// Variables from configuration of the app
+var cassandraKeyspace string
 var kafkaIp string
 var kafkaPort string
-var sparkMaster string
-var sparkHome string
 var cassandraHost string
 var minioHost string
-var waitGroup sync.WaitGroup
-
-var pysparkCassandraVersion = "0.3.5"
+var minio_port string
+var sparkHome string
+var sparkMaster string
+var spark_port string
+var alluxio_port string
+var pysparkCassandraVersion string
 var analysersPath = "/Users/Gabo/benchflow/analysers/analysers"
 var transformersPath = "/Users/Gabo/benchflow/data-transformers/data-transformers"
+var configurationsPath = "/app/configuration"
 
+// Sync group to prevent the app from terminating as long as consumers are listening on kafka
+var waitGroup sync.WaitGroup
+
+// Structures for storing the dependencies configurations
 type Configuration struct {
 	TransformersSettings []TransformerSetting `yaml:"transformers_settings"`
 	AnalysersSettings []AnalyserSetting `yaml:"analysers_settings"`
@@ -81,16 +103,15 @@ type KafkaMessage struct {
 	Collector_name string `json:"collector_name"`
 	}
 
+// Function that constructs and returns the arguments for a spark-submit command for a transformer script
 func constructTransformerSubmitArguments(ss SparkSubmit, experimentID string) []string {
 	var args []string
 	args = append(args, "--jars", sparkHome+"/pyspark-cassandra-assembly-"+pysparkCassandraVersion+".jar")
-	args = append(args, "--conf", "spark.cassandra.connection.host="+os.Getenv("CASSANDRA_IP"))
+	args = append(args, "--conf", "spark.cassandra.connection.host="+cassandraHost)
 	args = append(args, "--driver-class-path", sparkHome+"/pyspark-cassandra-assembly-"+pysparkCassandraVersion+".jar")
 	args = append(args, "--py-files", transformersPath+"/transformations/dataTransformations.py"+","+sparkHome+"/pyspark-cassandra-assembly-"+pysparkCassandraVersion+".jar")
 	args = append(args, "--files", ss.Files)
 	args = append(args, "--master", ss.SparkMaster)
-	// TODO: Move this in a configuration
-	args = append(args, "--conf", "spark.driver.memory=4g")
 	args = append(args, ss.Script)
 	args = append(args, ss.MinioHost)
 	args = append(args, ss.FileLocation)
@@ -102,6 +123,7 @@ func constructTransformerSubmitArguments(ss SparkSubmit, experimentID string) []
 	return args
 }
 
+// Function that creates a kafka consumer on a given topic name
 func kafkaConsumer(name string) consumergroup.ConsumerGroup {
 	config := consumergroup.NewConfig()
 	config.ClientID = "benchflow"
@@ -114,6 +136,7 @@ func kafkaConsumer(name string) consumergroup.ConsumerGroup {
 	return *consumer
 	}
 
+// Function that consumes messages from a topic and processes the messages, launching the transformer scripts and launching dependency checking
 func consumeFromTopic(t TransformerSetting) {
 	go func() {
 		consumer := kafkaConsumer(t.Topic)
@@ -144,7 +167,7 @@ func consumeFromTopic(t TransformerSetting) {
 					ss := SparkCommandBuilder.
 						Packages(s.Packages).
 						Script(s.Script).
-						Files(transformersPath+"/conf/data-transformers/"+msg.SUT_name+".data-transformers.yml").
+						Files(transformersPath+configurationsPath+"/data-transformers/"+msg.SUT_name+".data-transformers.yml").
 						PyFiles(s.PyFiles).
 						FileLocation("runs/"+k).
 						CassandraHost(cassandraHost).
@@ -156,7 +179,6 @@ func consumeFromTopic(t TransformerSetting) {
 						Build()
 					args := constructTransformerSubmitArguments(ss, msg.Experiment_id)
 					submitScript(args, s.Script)
-					//containerID := msg.Collector_name
 					containerID := msg.Container_id
 					meetRequirement(t.Topic, msg.Trial_id, msg.Experiment_id, "trial")
 					launchAnalyserScripts(msg.Trial_id, msg.Experiment_id, msg.SUT_name, msg.Total_trials_num, containerID, msg.Collector_name)
@@ -169,12 +191,13 @@ func consumeFromTopic(t TransformerSetting) {
 		}()
 }
 
-func submitAnalyser(scriptName string, script string, trialID string, experimentID string, SUTName string, containerID string, level string) {
+// Function that constructs the arguments for a spark-submit comand for an analyser script
+func constructAnalyserSubmitArguments(scriptName string, script string, trialID string, experimentID string, SUTName string, containerID string) []string {
 	var args []string
 	args = append(args, "--jars", sparkHome+"/pyspark-cassandra-assembly-"+pysparkCassandraVersion+".jar")
 	args = append(args, "--driver-class-path", sparkHome+"/pyspark-cassandra-assembly-"+pysparkCassandraVersion+".jar")
-	args = append(args, "--conf", "spark.cassandra.connection.host="+os.Getenv("CASSANDRA_IP"))
-	args = append(args, "--files", analysersPath+"/conf/analysers/"+SUTName+".analysers.yml")
+	args = append(args, "--conf", "spark.cassandra.connection.host="+cassandraHost)
+	args = append(args, "--files", configurationsPath+"/analysers/"+SUTName+".analysers.yml")
 	args = append(args, "--py-files", analysersPath+"/commons/commons.py,"+sparkHome+"/pyspark-cassandra-assembly-"+pysparkCassandraVersion+".jar")
 	args = append(args, "--master", "local[*]")
 	args = append(args, script)
@@ -182,13 +205,19 @@ func submitAnalyser(scriptName string, script string, trialID string, experiment
 	args = append(args, experimentID)
 	args = append(args, SUTName)
 	args = append(args, containerID)
-	fmt.Println(args)
+	return args
+	}
+
+// Function that submits an analyser script, and meets its requirement if it succeeds
+func submitAnalyser(scriptName string, script string, trialID string, experimentID string, SUTName string, containerID string, level string) {
+	args := constructAnalyserSubmitArguments(scriptName, script, trialID, experimentID, SUTName, containerID)
 	success := submitScript(args, script)
 	if success {
 		meetRequirement(scriptName, trialID, experimentID, level)
 	}
 }
 
+// Function that checks if the given requirements are met
 func checkRequirements(neededReqsString string, currentReqsString map[string]bool) bool {
 	reqMet := true
 	neededReqs := strings.Split(neededReqsString, ",")
@@ -203,14 +232,14 @@ func checkRequirements(neededReqsString string, currentReqsString map[string]boo
 	return reqMet
 }
 
+// Function that registers a given requirement as met
 func meetRequirement(req string, trialID string, experimentID string, level string) {
 	if level == "trial" {
 		if _, ok := reqTracker[trialID]; !ok {
 			reqTracker[trialID] = make(map[string]bool)
 		}
 		reqTracker[trialID][req] = true
-		}
-	if level == "experiment" {
+	} else if level == "experiment" {
 		if _, ok := reqTracker[experimentID]; !ok {
 			reqTracker[experimentID] = make(map[string]bool)
 		}
@@ -219,6 +248,7 @@ func meetRequirement(req string, trialID string, experimentID string, level stri
 	fmt.Println(reqTracker[trialID])
 	}
 
+// Function that checks if all scripts for a given trial have been concluded
 func isTrialComplete(trialID string) bool{
 	for _, sc := range allScripts {
 		if _, ok := reqTracker[trialID][sc]; !ok {
@@ -231,6 +261,7 @@ func isTrialComplete(trialID string) bool{
 	return true
 	}
 
+// Function that checks if all scripts for a given experiment have been concluded
 func isExperimentComplete(experimentID string) bool{
 	for _, sc := range allScripts {
 		if _, ok := reqTracker[experimentID][sc]; !ok {
@@ -243,6 +274,7 @@ func isExperimentComplete(experimentID string) bool{
 	return true
 	}
 
+// Function that checks for requirements and launches the analysers that meet them
 func launchAnalyserScripts(trialID string, experimentID string, SUTName string, totalTrials int, containerID string, collectorName string) {
 	currentReqs := reqTracker[trialID]
 	for r, scripts := range reqScripts {
@@ -287,13 +319,13 @@ func launchAnalyserScripts(trialID string, experimentID string, SUTName string, 
 		}
 	}
 
+// Function that submits a script with spark-submit and checks the output for errors
 func submitScript(args []string, script string) bool {
 	retries := 0
 	cmd := exec.Command(sparkHome+"/bin/spark-submit", args...)
 	for retries < 3 {
 		retries += 1
 		cmd.Stdout = os.Stdout
-		//cmd.Stderr = os.Stderr
 		errOutput := &bytes.Buffer{}
 		cmd.Stderr = errOutput
 		err := cmd.Start()
@@ -318,6 +350,7 @@ func submitScript(args []string, script string) bool {
 	return true
 	}
 
+// Checks if the output log for spark-submit contains errors
 func checkForErrors(errLog string) bool {
 	errString := strings.ToLower(errLog)
 	if strings.Contains(errString, "error") {
@@ -329,25 +362,45 @@ func checkForErrors(errLog string) bool {
 	return false
 	}
 
+// Main function, which registers configurations and starts the consumers
 func main() {
-	kafkaIp = os.Getenv("KAFKA_IP")
-	kafkaPort = os.Getenv("KAFKA_PORT")
-	sparkMaster = os.Getenv("SPARK_MASTER")
-	sparkHome = os.Getenv("SPARK_HOME")
-	cassandraHost = os.Getenv("CASSANDRA_IP")
-	minioHost = os.Getenv("MINIO_IP")
+	// Settings for viper
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/app/")
+	viper.AddConfigPath("./")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+	    panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 	
-	dat, err := ioutil.ReadFile("configuration/config2.yml")
+	// Getting app configuration with viper, uses ENV Variables instead of set
+	cassandraKeyspace = viper.GetString("cassandra_keyspace")
+	kafkaIp = viper.GetString("kafka_host")
+	kafkaPort = viper.GetString("kafka_port")
+	cassandraHost = viper.GetString("cassandra_host")
+	minioHost = viper.GetString("minio_host")
+	minio_port = viper.GetString("minio_port")
+	sparkHome = viper.GetString("spark_home")
+	sparkMaster = viper.GetString("spark_master")
+	spark_port = viper.GetString("spark_port")
+	alluxio_port = viper.GetString("alluxio_port")
+	pysparkCassandraVersion = viper.GetString("pyspark_cassandra_version")
+	analysersPath = viper.GetString("analysers_path")
+	transformersPath = viper.GetString("transformers_path")
+	
+	// Getting dependencies configuration and unmarshaling in defined structures
+	dat, err := ioutil.ReadFile("configuration/config.yml")
     if err != nil {
 			panic(err)
 			}
-	
 	err = yaml.Unmarshal(dat, &c)
 	if err != nil {
 			panic(err)
 			}
 	fmt.Println(c.AnalysersSettings)
 	
+	// Mapping the requirements string to the scripts associated with those requirements
 	for _, s := range c.AnalysersSettings {
 		reqScripts[s.Requirements] = s.Scripts
 		reqs := strings.Split(s.Requirements, ",")
@@ -361,13 +414,16 @@ func main() {
 	
 	fmt.Println(allRequirements)
 	
+	// Starts the wait group
 	waitGroup = sync.WaitGroup{}
 	
+	// Starts consumers
 	for _, sett := range c.TransformersSettings {
 		consumeFromTopic(sett)
 		waitGroup.Add(1)
 		}
 	
+	// Waits as long as consumers are running
 	waitGroup.Wait()
 	}
 
