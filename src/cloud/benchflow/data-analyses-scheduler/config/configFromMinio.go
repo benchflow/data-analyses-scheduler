d270f7eafcaec5f28372922de32dcb1f3637ff5e
Cerfoglg <gabrielecerfoglio225@gmail.com>
2016-08-18 16:06:30 +0200

    added hash to retrieve minio config

--- src/cloud/benchflow/data-analyses-scheduler/config/configFromMinio.go
+++ src/cloud/benchflow/data-analyses-scheduler/config/configFromMinio.go
@@ -3,9 +3,20 @@ package config
 import (
 	"github.com/minio/minio-go"
 	"strings"
+	"crypto/md5"
+	"encoding/hex"
 	"gopkg.in/yaml.v2"
 	. "cloud/benchflow/data-analyses-scheduler/vars"
 )
+
+// TODO: Take the commons one
+const numOfHashCharacters int = 4
+func hashKey(key string) string {
+	hasher := md5.New()
+    hasher.Write([]byte(key))
+    hashString := hex.EncodeToString(hasher.Sum(nil))
+	return (hashString[:numOfHashCharacters])
+	}
 
 // Retrieve the test configuration from Minio
 func TakeTestConfigFromMinio(experimentID string) (int, string, string, string, error) {
@@ -31,7 +42,9 @@ func TakeTestConfigFromMinio(experimentID string) (int, string, string, string, 
 	}
 	
 	// Path of the file
-	path := strings.Replace(experimentID, ".", "/", -1)
+	lastDotIndex := strings.LastIndex(experimentID, ".")
+	hash := hashKey(experimentID[:lastDotIndex])
+	path := hash+"/"+(strings.Replace(experimentID, ".", "/", -1))
 	
 	// Get object info
 	objInfo, err := minioClient.StatObject(TestsConfigBucket, path+"/"+TestsConfigName)
